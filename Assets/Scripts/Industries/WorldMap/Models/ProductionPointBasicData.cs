60d2cd1500be0da8a95aacc908e8421ce05a381b
Dmytrii Petrinko <dmytrii.petrinko@gmail.com>
2022-04-02 00:50:25 +0300

    Removed unnecessary data from point base data

--- Assets/Scripts/Industries/WorldMap/Models/ProductionPointBasicData.cs
+++ Assets/Scripts/Industries/WorldMap/Models/ProductionPointBasicData.cs
@@ -12,13 +12,9 @@ namespace Industries.WorldMap.Models
 		[SerializeField] private short m_Id;
 		[SerializeField] private string m_PointName;
 		[SerializeField] private string m_Description;
-		[SerializeField] private Vector3 m_Position;
-		[SerializeField] private GameObject m_Prefab;
 
 		public short Id => m_Id;
 		public string Name => m_PointName;
 		public string Description => m_Description;
-		public Vector3 Position => m_Position;
-		public GameObject Prefab => m_Prefab;
 	}
 }
